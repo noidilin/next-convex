257d8a4ca4228dd69a2f302cd2a13669d744dc87
noidilin <linganinja.0120@gmail.com>
2026-01-21 02:09:22 +0800

    feat(api): add next server action to handle image upload with convex api
    - first generate image url
    - POST request and retrieve storage ID for that image
    - save storage ID in db for future reference

--- app/action.ts
+++ app/action.ts
@@ -7,21 +7,41 @@ import { fetchAuthMutation } from '@/lib/auth-server'
 import { postSchema } from '@/lib/schemas'
 
 export async function createBlogAction(value: z.infer<typeof postSchema>) {
-  const parsed = postSchema.safeParse(value)
-  if (!parsed.success) throw new Error('something went wrong')
+  try {
+    const parsed = postSchema.safeParse(value)
+    if (!parsed.success) throw new Error('something went wrong')
 
-  // NOTE: server function won't handle token for you automatically like `useMutation` did
-  // const token = await getToken();
-  // await fetchMutation(api.posts.createPost, {
-  //   body: parsed.data.content,
-  //   title: parsed.data.title,
-  // }, { token },
-  // )
+    if (!parsed.data.image) return { error: 'Image is required' }
+    // NOTE: server function won't handle token for you automatically like `useMutation` did
+    // const token = await getToken();
+    // await fetchMutation(api.posts.createPost, {
+    //   body: parsed.data.content,
+    //   title: parsed.data.title,
+    // }, { token },
+    // )
 
-  await fetchAuthMutation(api.posts.createPost, {
-    body: parsed.data.content,
-    title: parsed.data.title,
-  })
+    // NOTE: three steps convention for upload image to convex
+    // 1. generate image upload url (like endpoint)
+    const imageUrl = await fetchAuthMutation(api.posts.generateImageUploadUrl)
+    // 2. POST method to that endpoint
+    const uploadResult = await fetch(imageUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': parsed.data.image.type,
+      },
+      body: parsed.data.image,
+    })
+    if (!uploadResult.ok) return { error: 'Failed to upload image' }
+    // retrieve the storage ID and store it within the db
+    const { storageId } = await uploadResult.json()
+    await fetchAuthMutation(api.posts.createPost, {
+      body: parsed.data.content,
+      title: parsed.data.title,
+      imageStorageId: storageId,
+    })
+  } catch {
+    return { error: 'Failed to create post' }
+  }
 
   return redirect('/')
 }
